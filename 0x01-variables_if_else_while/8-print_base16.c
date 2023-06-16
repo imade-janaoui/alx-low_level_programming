8b62176f16548c5f371a562c84c51912c028c5e5
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-16 22:26:45 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-    int i;
-    char alphabet;
-
-    for (i = 0; i < 10; i++)
+	int i;
+	char alphabet;
+	for (i = 0; i < 10; i++)
 	    putchar(( i% 10) + '0');
-
-    for (alphabet = 'a'; alphabet <= 'f' ; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'f' ; alphabet++)
 	    putchar(alphabet);
-    putchar('\n');
-
-    return 0;
+	putchar('\n');
+	return 0;
 }
