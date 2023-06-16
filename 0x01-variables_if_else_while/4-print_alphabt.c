e9c9682b4270cc17567dc2af1dffe91e0a438eec
imade-janaoui <imadejanaoui@gmail.com>
2023-06-16 12:56:10 -0700

    modification

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints the alphabet in lowercase
+ * main - Write a program that prints the alphabet in lowercase.
  *
  * Return : Always 0
  */
