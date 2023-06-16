ec4882cdcf015b46bb69b9792b1b5cfef0b2f028
imade-janaoui <imadejanaoui@gmail.com>
2023-06-16 14:12:41 -0700

    add main description

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Write a program that prints the lowercase alphabet in reverse.
+ * 
+ * Return: Always 0
+ */
 int main(void)
 {
     char alphabet = 'z';
-    while (alphabet >='a')
+
+    while (alphabet >= 'a')
     {
      putchar(alphabet);
     alphabet--;
