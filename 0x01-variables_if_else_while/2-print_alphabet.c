81e219aa045b666bb0773385b9512dc775475fe7
imade-janaoui <imadejanaoui@gmail.com>
2023-06-16 12:32:07 -0700

    add description

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /* more headers goes there */
-
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ *
+ * Return: Always 0
 /* betty style doc for function main goes there */
 int main(void)
 {
