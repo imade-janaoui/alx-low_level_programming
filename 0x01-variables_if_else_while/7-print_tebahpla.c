51c68fd07f4fc59d998c4f002c727445d9ac49ef
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-16 22:15:15 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-    char alphabet = 'z';
-
-    while (alphabet >= 'a')
-    {
-     putchar(alphabet);
-    alphabet--;
-    }
+	char alphabet = 'z';
+	
+	while (alphabet >= 'a'){
+		putchar(alphabet);
+		alphabet--;
+	}
 	putchar('\n');
-
-
-    return 0;
+	return 0;
 }
