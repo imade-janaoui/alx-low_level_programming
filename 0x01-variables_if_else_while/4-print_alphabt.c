c1802b559a9603144ba6f998d7be8714427a36da
imade-janaoui <imadejanaoui@gmail.com>
2023-06-16 02:52:11 -0700

    modification

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,5 +14,6 @@ int main(void)
 		if(letter != 'q' && letter != 'e')
 			putchar(letter);
 	}
+	printf("\n");
 	return 0;
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,5 +10,6 @@ int main(void)
 	int i=0;
 	for ( i=0;i<10;i++)
 		printf("%d",i);
+	printf("\n");
 	return 0;
 }
