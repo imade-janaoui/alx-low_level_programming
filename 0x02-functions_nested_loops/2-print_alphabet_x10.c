aae6e72db760580d45c971b0b3899465a7cc7761
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-20 19:29:09 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Write a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - Write a function that prints 10 times the alphabet
  *
  * Return: void
  */
