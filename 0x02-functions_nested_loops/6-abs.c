78c9152d54585feceea135f46df1cd904aaf812c
imade-janaoui <imadejanaoui@gmail.com>
2023-06-20 12:37:20 -0700

    add progrmme to return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Write a function that computes the absolute value of an integer
+ * @num: variable to test 
+ * Return: Nothing
+ */
+
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
