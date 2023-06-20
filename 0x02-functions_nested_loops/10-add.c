ae87c3c7aa762eb30885f00ec258e79d689befe0
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-20 23:22:10 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Write a function that adds two integers and returns the result.
- * @i: The first number to be added.
- * @j: The second number to be added.
+ * @a: The first number to be added.
+ * @b: The second number to be added.
  * Return: the sum
  */
 int add(int b, int a)
