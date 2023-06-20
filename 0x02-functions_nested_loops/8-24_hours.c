f9589a2841fdfe15c3d8e07bfed2a3c546d98df7
imade-janaoui <imadejanaoui@gmail.com>
2023-06-20 15:08:12 -0700

    jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - Write a function that prints.
+ *  every minute of the day of Jack Bauer
+ *
+ *  Return: the sum of two numbers.
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
