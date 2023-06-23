2b42f81b4cc668f7dbda1d4fab401801939e05b9
imade-janaoui <imadejanaoui@gmail.com>
2023-06-23 11:42:29 -0700

    add changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	i = '0';
-	while (i <= "10")
+	i = 0;
+	while (i <= 10)
 	{
-		for (j = '0'; j <= "14"; j++)
-			_putchar(j);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
 		_putchar('\n');
 		i++;
 	}
