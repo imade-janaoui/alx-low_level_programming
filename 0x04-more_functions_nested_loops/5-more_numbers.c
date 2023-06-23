d4e8135f3123db7bddacbf0187555df4cf84d13a
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-23 22:16:56 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i, j;
 	
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 11; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
