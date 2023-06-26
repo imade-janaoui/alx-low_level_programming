d232bc7fbfdfd4f90419c20f68b39a255ca50bb9
imade-janaoui <imadejanaoui@gmail.com>
2023-06-26 14:17:31 -0700

    revese

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 	while (s[i] != '\0')
 		i++;
 	len = i;
-	for (i = len - 1; j >= 0; j--)
-		_putchar(s[j]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('n');
 }
