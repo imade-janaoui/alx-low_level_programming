ef5e84a112c61efbe01de13eb9c94ed69a8d63ba
Imade Janaoui <80173800+imade-janaoui@users.noreply.github.com>
2023-06-26 23:14:57 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,18 @@ void print_rev(char *s)
 	int i, j, len;
 
 	i = 0;
+
 	while (s[i] != '\0')
+	{
 		i++;
+	}
+
 	len = i;
+
 	for (j = len - 1; j >= 0; j--)
+	{
 		_putchar(s[j]);
-	_putchar('n');
+	}
+
+	_putchar('\n');
 }
